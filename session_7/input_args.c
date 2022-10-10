8b6c89e700a97dacce8a1e7987f7fb2d3b0a824e
Robotawi <mohamed.s.raessa@gmail.com>
2022-10-10 22:55:56 +0900

    Stop execution to check the process

--- session_7/input_args.c
+++ session_7/input_args.c
@@ -2,7 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Got %d arguments");
+    printf("Got %d arguments", argc);
+
+    getchar();//to stop execution and check things like pid
+
     for (int i = 0; i < argc; i++){
         printf("Arg number %d has a value of %s \n", i, argv[i]);
     }
